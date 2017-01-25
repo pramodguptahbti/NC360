c0de734c45a87b08339921421384c6f40e9fb69b
RAC IT SOLUTIONS PVT.LTD <racitsolutionspvtltd@racs-mac-mini.local>
2017-01-25 15:53:47 +0530

    Initial Commit

---
+++ 360NC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  360NC
+//
+//  Created by RAC IT SOLUTIONS PVT.LTD on 25/01/17.
+//  Copyright © 2017 NetConnect. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 360NC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  360NC
+//
+//  Created by RAC IT SOLUTIONS PVT.LTD on 25/01/17.
+//  Copyright © 2017 NetConnect. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
